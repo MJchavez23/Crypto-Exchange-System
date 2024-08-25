e47458ab0464b308eafee4d195ba745c41f4e39b
Mateo <mchavez.castillo23@gmail.com>
2024-08-25 17:27:27 -0500

    Wallet Model

---
+++ src/com/globant/model/Wallet.java
@@ -0,0 +1,10 @@
+package com.globant.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Wallet {
+    private String userId;
+    private int balanceFiat;
+    private Map<String, double[]> cryptoCurrencies;
+}
