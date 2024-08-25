cdfd867428ea03d2b81b3ba8204770d80665594d
Mateo <mchavez.castillo23@gmail.com>
2024-08-25 17:31:55 -0500

    Adding order model

---
+++ src/com/globant/model/OrderModel.java
@@ -0,0 +1,10 @@
+package com.globant.model;
+
+public class OrderModel {
+    private int orderId;
+    private int userid;
+    private String cryptoCurrencyName;
+    private double amount;
+    private double price;
+    private boolean isBuying;
+}
