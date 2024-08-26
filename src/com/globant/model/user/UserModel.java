cec218067715be6c4155a05284ad9474ec8b5e3b
Mateo <mchavez.castillo23@gmail.com>
2024-08-26 15:00:54 -0500

    Register method

--- src/com/globant/model/user/UserModel.java
+++ src/com/globant/model/user/UserModel.java
@@ -1,13 +1,27 @@
 package com.globant.model.user;
 
-import com.globant.model.WalletModel;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+
 
 public class UserModel {
 
     private static final String CSV_FILE = "users.csv";
 
 
-    public boolean registerUser(User newUser){
-
+    public boolean registerUser(User user){
+        try(PrintWriter pw = new PrintWriter(new FileWriter(CSV_FILE, true))) {
+            pw.printf("\\\"%d\\\",\\\"%s\\\",\\\"%s\\\",\\\"%s\\\"%n\"",
+                    user.getUserId(),
+                    user.getUserName().replace("\"", "\"\""),
+                    user.getEmail().replace("\"", "\"\""),
+                    user.getPassword().replace("\"", "\"\""));
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
     }
 }
