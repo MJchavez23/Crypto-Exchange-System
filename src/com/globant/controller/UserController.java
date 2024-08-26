63db64abd705214944143f2b664f342c40d3f4f6
Mateo <mchavez.castillo23@gmail.com>
2024-08-26 14:11:26 -0500

    UserController

--- src/com/globant/controller/UserController.java
+++ src/com/globant/controller/UserController.java
@@ -1,4 +1,19 @@
 package com.globant.controller;
 
+import com.globant.model.UserModel;
+
+import java.util.Random;
+
 public class UserController {
+    private final Random rand = new Random();
+    private UserModel model;
+
+
+    public UserController(UserModel model) {
+        this.model = model;
+    }
+
+    public boolean register(String username, String password, String email){
+
+    }
 }
