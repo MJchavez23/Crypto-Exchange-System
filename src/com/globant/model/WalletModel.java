90500c18e96aa104277bf536edf5280782bcefd9
Mateo <mchavez.castillo23@gmail.com>
2024-08-26 15:17:26 -0500

    new wallet method

--- src/com/globant/model/WalletModel.java
+++ src/com/globant/model/WalletModel.java
@@ -1,14 +1,38 @@
 package com.globant.model;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.Map;
+
 
 public class WalletModel {
     private int userId;
-    private int balanceFiat;
+    private double balanceFiat;
     private HashMap<String, Double> cryptoCurrencies;
+    private static final String CSV_FILE = "wallet.csv";
 
-    public boolean newWallet(int userId, int balanceFiat, HashMap<String, Double> cryptoCurrencies){
+    public void newWallet(int userId, double balanceFiat, HashMap<String, Double> cryptoCurrencies){
+        try(PrintWriter pw = new PrintWriter(new FileWriter(CSV_FILE, true))) {
+             pw.printf("%d,%.2f",
+                userId,
+                balanceFiat
+            );
+             pw.print(",");
+            HashMap<String, Double> crypto = cryptoCurrencies;
+            StringBuilder cryptoBuilder = new StringBuilder();
+            for (Map.Entry<String, Double> entry : crypto.entrySet()) {
+                if (!cryptoBuilder.isEmpty()) {
+                    cryptoBuilder.append(";");
+                }
+                cryptoBuilder.append(entry.getKey()).append(":").append(entry.getValue());
+            }
+            pw.print("\"" + cryptoBuilder.toString() + "\"%n");
 
+        } catch (IOException e) {
+
+        }
     }
 
 }
