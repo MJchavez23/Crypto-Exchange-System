3b2bc21853aec2700a52117fcdb6b10205d5140a
Mateo <mchavez.castillo23@gmail.com>
2024-08-26 14:03:19 -0500

    Adding constructor

--- src/com/globant/model/UserModel.java
+++ src/com/globant/model/UserModel.java
@@ -1,9 +1,21 @@
 package com.globant.model;
 
+import java.util.Random;
+
 public class UserModel {
+    private static final Random rand = new Random();
+
     private int userId;
     private String userName;
     private String email;
     private String password;
     private WalletModel wallet;
+
+    public UserModel(int userId, String userName, String email, String password, WalletModel wallet) {
+        this.userId = userId;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.wallet = wallet;
+    }
 }
