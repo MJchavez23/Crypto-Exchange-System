7ff94873b2fd318f400d805fba5a0ea8d59b2ba4
Mateo <mchavez.castillo23@gmail.com>
2024-08-26 14:50:19 -0500

    add registerUser method

---
+++ src/com/globant/model/user/UserModel.java
@@ -0,0 +1,13 @@
+package com.globant.model.user;
+
+import com.globant.model.WalletModel;
+
+public class UserModel {
+
+    private static final String CSV_FILE = "users.csv";
+
+
+    public boolean registerUser(User newUser){
+
+    }
+}
