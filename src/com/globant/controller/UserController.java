eb915f5e00439efd1354714bb07652431efdb292
Mateo <mchavez.castillo23@gmail.com>
2024-08-26 14:50:11 -0500

    add registerUser method

--- src/com/globant/controller/UserController.java
+++ src/com/globant/controller/UserController.java
@@ -1,16 +1,35 @@
 package com.globant.controller;
 
-import com.globant.model.UserModel;
+import com.globant.model.WalletModel;
+import com.globant.model.user.User;
+import com.globant.model.user.UserModel;
 
+import java.util.HashMap;
 import java.util.Random;
 
 public class UserController {
     private final Random rand = new Random();
     private UserModel model;
+    private WalletModel wallet;
 
 
     public UserController(UserModel model) {
         this.model = model;
+        this.wallet = new WalletModel();
+    }
+
+
+    public boolean register(String username, String password, String email){
+        int newUserId = rand.nextInt();
+        HashMap<String, Double> cryptoCurrencies = new HashMap<>();
+        cryptoCurrencies.put("BitCoin", 0.0);
+        cryptoCurrencies.put("Ethereum", 0.0);
+        boolean newWallet = wallet.newWallet(newUserId, 0, cryptoCurrencies);
+
+        User newUser = new User(newUserId, username, email, password);
+        boolean check = model.registerUser(newUser);
+
+        return check;
     }
 
 }
