09730bad29c5678d9c21eaad2f0c674d6c9941f0
Mateo <mchavez.castillo23@gmail.com>
2024-08-26 14:50:54 -0500

    adding new wallet method

--- src/com/globant/model/WalletModel.java
+++ src/com/globant/model/WalletModel.java
@@ -6,4 +6,9 @@ public class WalletModel {
     private int userId;
     private int balanceFiat;
     private HashMap<String, Double> cryptoCurrencies;
+
+    public boolean newWallet(int userId, int balanceFiat, HashMap<String, Double> cryptoCurrencies){
+
+    }
+
 }
