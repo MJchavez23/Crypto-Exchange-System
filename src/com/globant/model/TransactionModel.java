07307868f80597fdac7d273449dfdf5416b438b0
Mateo <mchavez.castillo23@gmail.com>
2024-08-26 13:58:11 -0500

    Transaction Model

---
+++ src/com/globant/model/TransactionModel.java
@@ -0,0 +1,13 @@
+package com.globant.model;
+
+import java.util.Date;
+
+public class TransactionModel {
+    private int transactionId;
+    private int buyerId;
+    private int sellerId;
+    private String cryptoCurrencyName;
+    private double amount;
+    private double tradePrice;
+    private Date transactionDate;
+}
