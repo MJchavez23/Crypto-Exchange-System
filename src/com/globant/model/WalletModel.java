f668ede3786167c06e180945da7cd28fb431b311
Mateo <mchavez.castillo23@gmail.com>
2024-08-27 14:42:52 -0500

    Changing the new wallet model

--- src/com/globant/model/WalletModel.java
+++ src/com/globant/model/WalletModel.java
@@ -14,23 +14,11 @@ public class WalletModel {
     private HashMap<String, Double> cryptoCurrencies;
     private static final String CSV_FILE = "wallet.csv";
 
-    public void newWallet(int userId, double balanceFiat, HashMap<String, Double> cryptoCurrencies){
+    public void newWallet(int userId, double balanceFiat){
         try(PrintWriter pw = new PrintWriter(new FileWriter(CSV_FILE, true))) {
-             pw.printf("%d,%.2f",
-                userId,
-                balanceFiat
-            );
-             pw.print(",");
-            HashMap<String, Double> crypto = cryptoCurrencies;
-            StringBuilder cryptoBuilder = new StringBuilder();
-            for (Map.Entry<String, Double> entry : crypto.entrySet()) {
-                if (!cryptoBuilder.isEmpty()) {
-                    cryptoBuilder.append(";");
-                }
-                cryptoBuilder.append(entry.getKey()).append(":").append(entry.getValue());
-            }
-            pw.print("\"" + cryptoBuilder.toString() + "\"%n");
-
+             pw.printf("%d, %.2f", userId, balanceFiat);
+             pw.print(",0,0");
+             pw.println();
         } catch (IOException e) {
 
         }
