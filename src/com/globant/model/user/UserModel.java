de6b54278e2dc0b99b8d23b2e64bd0731cac42dc
Mateo <mchavez.castillo23@gmail.com>
2024-08-27 14:12:19 -0500

    Changes

--- src/com/globant/model/user/UserModel.java
+++ src/com/globant/model/user/UserModel.java
@@ -9,17 +9,15 @@ public class UserModel {
     private static final String CSV_FILE = "src/com/globant/model/user/users.csv";
 
 
-    public boolean registerUser(User user){
+    public void registerUser(User user){
         try (PrintWriter pw = new PrintWriter(new FileWriter(CSV_FILE, true))) {
-            // Write user details to the CSV file without extra escaping
             pw.printf("%d,\"%s\",\"%s\",\"%s\"%n",
                     user.getUserId(),
                     user.getUserName().replace("\"", "\"\""),
                     user.getEmail().replace("\"", "\"\""),
                     user.getPassword().replace("\"", "\"\""));
-            return true;
         } catch (IOException e) {
-            return false;
+
         }
     }
 }
