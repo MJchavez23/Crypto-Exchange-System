00f359332c713fad80832386e06b5ec64abf43b1
Mateo <mchavez.castillo23@gmail.com>
2024-08-27 14:03:10 -0500

    adding root controller

--- src/com/globant/controller/RootController.java
+++ src/com/globant/controller/RootController.java
@@ -1,9 +1,32 @@
 package com.globant.controller;
 
+import com.globant.model.WalletModel;
+import com.globant.model.user.UserModel;
+import com.globant.view.ConsoleView;
+
 public class RootController {
     private UserController userController;
     private WalletController walletController;
+    private ConsoleView view;
+    public RootController(UserModel userModel, WalletModel walletModel, ConsoleView view){
+        this.view = view;
+        this.userController = new UserController(userModel, view);
+    }
 
-    public RootController(UserController userController, WalletController walletController){
+    public void run(){
+        while(true){
+            int choice = view.getUserChoice();
+            switch(choice){
+                case 1:
+                    this.userController.executeLogin();
+                    break;
+                case 2:
+                    this.userController.executeRegister();
+                    break;
+
+                default:
+                    view.showError("Invalid choice");
+            }
+        }
     }
 }
