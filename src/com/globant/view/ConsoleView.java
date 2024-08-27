3a086fabf045412529fb5ab12a137adeef1ad9ce
Mateo <mchavez.castillo23@gmail.com>
2024-08-27 13:35:43 -0500

    Console View.... methods login page and getuserChoice

---
+++ src/com/globant/view/ConsoleView.java
@@ -0,0 +1,42 @@
+package com.globant.view;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class ConsoleView {
+    private final Scanner scanner;
+
+    public ConsoleView() {this.scanner = new Scanner(System.in);}
+
+    public int getUserChoice(){
+        System.out.println("----- WELCOME -----");
+        System.out.println("CRYPTO EXCHANGE SYSTEM");
+        System.out.println("1.LOGIN");
+        System.out.println("1.REGISTRATION");
+        try{
+            return this.scanner.nextInt();
+        }catch(InputMismatchException e){
+            this.scanner.nextLine();
+            return -1;
+        }
+    }
+
+    public List<String> loginPage(){
+            try{
+                System.out.println("Please enter your username: ");
+                String username = this.scanner.nextLine();
+                System.out.println("Please enter your password: ");
+                String password = this.scanner.nextLine();
+                List<String> data = new ArrayList<String>();
+                data.add(username);
+                data.add(password);
+                return data;
+            }catch(InputMismatchException e) {
+                System.out.println("Invalid username or password");
+                return loginPage();
+            }
+    }
+
+}
