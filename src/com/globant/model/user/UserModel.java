066ec24a672b5e2e1f1f95af05b17d42061a007e
Mateo <mchavez.castillo23@gmail.com>
2024-08-27 15:19:51 -0500

    User model

--- src/com/globant/model/user/UserModel.java
+++ src/com/globant/model/user/UserModel.java
@@ -3,6 +3,8 @@ package com.globant.model.user;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class UserModel {
 
@@ -19,5 +21,35 @@ public class UserModel {
         } catch (IOException e) {
 
         }
+    }
+
+
+    public User loginUser(String username, String password){
+        try (BufferedReader br = new BufferedReader(new FileReader(CSV_FILE))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+
+                String[] parts = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+
+                if (parts.length >= 4) {
+                    String csvUserId = parts[0].replace("\"", "");
+                    String csvUsername = parts[1].replace("\"", "");
+                    String csvPassword = parts[2].replace("\"", "");
+                    String csvEmail = parts[3].replace("\"", "");
+
+
+                    if (username.equals(csvUsername) && password.equals(csvPassword)) {
+                        int userId = Integer.parseInt(csvUserId);
+                        User newUser = new User(userId, csvUsername, csvEmail, csvPassword);
+                        return newUser;
+                    }
+                }
+            }
+        } catch (IOException e) {
+
+        }
+
+        System.out.println("User dont match");
+        return null;
     }
 }
