5b65691161a934711d49b619c7da0bc05c24931c
Mateo <mchavez.castillo23@gmail.com>
2024-08-27 14:02:58 -0500

    controllers for the login and register page

--- src/com/globant/controller/UserController.java
+++ src/com/globant/controller/UserController.java
@@ -3,32 +3,47 @@ package com.globant.controller;
 import com.globant.model.WalletModel;
 import com.globant.model.user.User;
 import com.globant.model.user.UserModel;
+import com.globant.view.ConsoleView;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
 
 public class UserController {
     private final Random rand = new Random();
     private UserModel model;
     private WalletModel wallet;
+    private ConsoleView view;
 
-
-    public UserController(UserModel model) {
+    public UserController(UserModel model, ConsoleView view) {
+        this.view = view;
         this.model = model;
         this.wallet = new WalletModel();
     }
 
+    public void executeRegister(){
+        List<String> data = view.registrationPage();
+        try{
+            register(data.get(0), data.get(1), data.get(2));
+            executeLogin();
+        }catch(Exception e){
+            view.showError("Register Error");
+        }
+    }
 
-    public boolean register(String username, String password, String email){
-        int newUserId = rand.nextInt();
-        HashMap<String, Double> cryptoCurrencies = new HashMap<>();
-        cryptoCurrencies.put("BitCoin", 0.0);
-        cryptoCurrencies.put("Ethereum", 0.0);
-        wallet.newWallet(newUserId, 0.0, cryptoCurrencies);
-        User newUser = new User(newUserId, username, email, password);
-        boolean result = model.registerUser(newUser);
 
-        return result;
+    public void executeLogin(){
+
+    }
+
+    public void register(String username, String password, String email){
+            int newUserId = rand.nextInt();
+            HashMap<String, Double> cryptoCurrencies = new HashMap<>();
+            cryptoCurrencies.put("BitCoin", 0.0);
+            cryptoCurrencies.put("Ethereum", 0.0);
+            wallet.newWallet(newUserId, 0.0, cryptoCurrencies);
+            User newUser = new User(newUserId, username, email, password);
+            model.registerUser(newUser);
     }
 
 }
