3bf4c9598fd4d99192df52f88c59b80a21c1e457
Mateo <mchavez.castillo23@gmail.com>
2024-08-27 15:19:57 -0500

    Changes

--- src/com/globant/controller/WalletController.java
+++ src/com/globant/controller/WalletController.java
@@ -6,8 +6,12 @@ import com.globant.view.ConsoleView;
 public class WalletController {
     private final WalletModel model;
     private final ConsoleView view;
+
     public WalletController(WalletModel model, ConsoleView view) {
         this.model = model;
         this.view = view;
     }
+
+
+
 }
