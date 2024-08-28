99de248226a7c0a7f8a019af412008eed88d8079
Mateo <mchavez.castillo23@gmail.com>
2024-08-28 08:02:36 -0500

    add method setWallet

--- src/com/globant/model/user/User.java
+++ src/com/globant/model/user/User.java
@@ -1,11 +1,14 @@
 package com.globant.model.user;
 
+
+import com.globant.model.wallet.Wallet;
+
 public class User {
     private final int userId;
     private final String userName;
     private final String email;
     private final String password;
-
+    private Wallet wallet;
 
     public User(int userId, String userName, String email, String password) {
         this.userId = userId;
@@ -13,6 +16,15 @@ public class User {
         this.email = email;
         this.password = password;
     }
+
+    public void setWallet(int userId, Double balanceFiat) {
+        this.wallet = new Wallet(userId, balanceFiat);
+    }
+
+    public double getWalletBalance() {
+        return wallet.getBalanceFiat();
+    }
+
 
     public int getUserId() {
         return userId;
