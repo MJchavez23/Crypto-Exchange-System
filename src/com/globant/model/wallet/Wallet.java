9e257e614c2ee5d46c684a29c6e95ff48068a8c7
Mateo <mchavez.castillo23@gmail.com>
2024-08-28 07:58:09 -0500

    New wallet class

---
+++ src/com/globant/model/wallet/Wallet.java
@@ -0,0 +1,16 @@
+package com.globant.model.wallet;
+
+import java.util.HashMap;
+
+public class Wallet {
+    private int userId;
+    private double balanceFiat;
+    private HashMap<String, Double> cryptoCurrencies;
+
+
+    public Wallet(int userId, double balanceFiat) {
+        this.userId = userId;
+        this.balanceFiat = balanceFiat;
+        this.cryptoCurrencies = new HashMap<>();
+    }
+}
