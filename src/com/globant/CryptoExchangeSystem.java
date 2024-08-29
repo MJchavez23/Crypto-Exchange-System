5cbf12f39f41ed6e248df9d4ac62e34d5c01bdc9
Mateo <mchavez.castillo23@gmail.com>
2024-08-29 13:03:33 -0500

    minor changes

--- src/com/globant/CryptoExchangeSystem.java
+++ src/com/globant/CryptoExchangeSystem.java
@@ -3,14 +3,15 @@ package com.globant;
 import com.globant.controller.RootController;
 import com.globant.model.wallet.WalletModel;
 import com.globant.model.user.UserModel;
+import com.globant.service.UserService;
 import com.globant.view.ConsoleView;
 
 public class CryptoExchangeSystem {
     public static void main(String[] args) {
-        UserModel userModel = new UserModel();
+        UserService userService = new UserService();
         WalletModel walletModel = new WalletModel();
         ConsoleView consoleView = new ConsoleView();
-        RootController rootController = new RootController(userModel, walletModel, consoleView);
+        RootController rootController = new RootController(userService, walletModel, consoleView);
 
         rootController.run();
         consoleView.close();
