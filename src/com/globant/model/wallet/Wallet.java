b1001cf29dd6281fe6a16fb3a487cbfa5f583b8b
Mateo <mchavez.castillo23@gmail.com>
2024-08-29 13:51:49 -0500

    add deposit method and add variable for the cryptocurrencies

--- src/com/globant/model/wallet/Wallet.java
+++ src/com/globant/model/wallet/Wallet.java
@@ -5,16 +5,22 @@ import java.util.HashMap;
 public class Wallet {
     private int userId;
     private double balanceFiat;
-    private HashMap<String, Double> cryptoCurrencies;
+    private double bitCoinBalance;
+    private double ethereumBalance;
 
 
     public Wallet(int userId, double balanceFiat) {
         this.userId = userId;
         this.balanceFiat = balanceFiat;
-        this.cryptoCurrencies = new HashMap<>();
+        this.bitCoinBalance = 0;
+        this.ethereumBalance = 0;
     }
 
     public double getBalanceFiat() {
         return balanceFiat;
+    }
+
+    public void depositFiat(double amount) {
+        balanceFiat = balanceFiat + amount;
     }
 }
