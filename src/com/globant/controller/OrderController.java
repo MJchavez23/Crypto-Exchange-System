a5b8493582fb6db4a598561ec8c422aeac3d4465
Mateo <mchavez.castillo23@gmail.com>
2024-08-30 17:12:20 -0500

    Order controller and service

---
+++ src/com/globant/controller/OrderController.java
@@ -0,0 +1,20 @@
+package com.globant.controller;
+
+import com.globant.service.OrderService;
+import com.globant.view.ConsoleView;
+
+public class OrderController {
+
+    private ConsoleView view;
+    private OrderService orderService;
+    public OrderController(OrderService orderService, ConsoleView view) {
+        this.orderService = orderService;
+        this.view = view;
+    }
+
+
+    public void placeSellOrder(){
+        String[] data = view.placeSellOrderPage();
+        orderService.placeSellOrder(data);
+    }
+}
---
+++ src/com/globant/service/OrderService.java
@@ -0,0 +1,14 @@
+package com.globant.service;
+
+import com.globant.model.OrderModel;
+
+public class OrderService {
+    private final OrderModel model;
+
+    public OrderService() {
+        model = new OrderModel();
+    }
+
+    public void placeSellOrder(String[] data) {
+    }
+}
