2cd5335db43f23578b7b20bd6ecd0ec53c43d382
Mateo <mchavez.castillo23@gmail.com>
2024-09-01 14:38:14 -0500

    creating save transaction method

--- src/com/globant/model/transaction/TransactionService.java
+++ src/com/globant/model/transaction/TransactionService.java
@@ -1,4 +1,16 @@
 package com.globant.model.transaction;
 
+import com.globant.model.user.User;
+
 public class TransactionService {
+    private TransactionModel model;
+
+    public TransactionService() {
+        this.model = new TransactionModel();
+    }
+
+    public void saveSellTransaction(User user, String cryptoName, double amount, double price, int isBuying){
+        int sellerId = user.getUserId();
+        model.saveSellTransaction(sellerId, cryptoName, amount, price, isBuying);
+    }
 }
