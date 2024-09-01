db6101ef3db30b1047bb1c9fc7cf2cdf738ff1e5
Mateo <mchavez.castillo23@gmail.com>
2024-09-01 15:19:00 -0500

    add method

--- src/com/globant/CryptoExchangeSystem.java
+++ src/com/globant/CryptoExchangeSystem.java
@@ -2,6 +2,7 @@ package com.globant;
 
 import com.globant.controller.RootController;
 import com.globant.model.orders.OrderModel;
+import com.globant.model.transaction.TransactionService;
 import com.globant.model.wallet.WalletModel;
 import com.globant.service.OrderService;
 import com.globant.service.UserService;
@@ -11,8 +12,9 @@ public class CryptoExchangeSystem {
     public static void main(String[] args) {
         UserService userService = new UserService();
         OrderService orderService = new OrderService();
+        TransactionService transactionService = new TransactionService();
         ConsoleView consoleView = new ConsoleView();
-        RootController rootController = new RootController(userService, orderService, consoleView);
+        RootController rootController = new RootController(userService, orderService, transactionService, consoleView);
         rootController.run();
         consoleView.close();
     }
