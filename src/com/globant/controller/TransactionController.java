90c3c5468dc89f8f2ba90775bd9c277f794beaa1
Mateo <mchavez.castillo23@gmail.com>
2024-09-01 15:18:35 -0500

    controller for transaction

---
+++ src/com/globant/controller/TransactionController.java
@@ -0,0 +1,26 @@
+package com.globant.controller;
+
+import com.globant.model.transaction.TransactionService;
+import com.globant.model.user.User;
+import com.globant.view.ConsoleView;
+
+import java.util.Arrays;
+
+public class TransactionController {
+
+    private final TransactionService transactionService;
+    private final ConsoleView view;
+
+    public TransactionController(TransactionService transactionService, ConsoleView view) {
+        this.transactionService = transactionService;
+        this.view = view;
+    }
+
+    public void showUserTransactions(User user) {
+        String[] transactions = transactionService.getUserTransaction(user);
+        for(String transaction : transactions) {
+            String[] parts = transaction.split(",");
+            view.showTransaction(parts[1], parts[2], parts[3], parts[4]);
+        }
+    }
+}
