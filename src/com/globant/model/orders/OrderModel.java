360f24de8c71d8cb4590c38b0a8fd03f43ecb393
Mateo <mchavez.castillo23@gmail.com>
2024-09-02 15:34:08 -0500

    functions to get sell orders and complete the orders

--- src/com/globant/model/orders/OrderModel.java
+++ src/com/globant/model/orders/OrderModel.java
@@ -1,13 +1,19 @@
 package com.globant.model.orders;
 
 import com.globant.model.CsvManager;
-import com.globant.model.transaction.TransactionModel;
 
 public class OrderModel {
-    private final TransactionModel transaction = new TransactionModel();
     private final CsvManager csvManager = new CsvManager();
 
     public void setSellOrder(int orderId, int userid, String cryptoCurrencyName, double amount, double price){
         csvManager.writeSellOrder(orderId, userid, cryptoCurrencyName, amount, price);
+    }
+
+    public String[] getSellOrder(int userId, String currencyName, double amount, double price){
+        return csvManager.getSellOrder(userId, currencyName, amount, price);
+    }
+
+    public void completeOrder(int userId, double newPrice){
+        csvManager.updateSellerWallet(userId, newPrice);
     }
 }
