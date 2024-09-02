91675790b93afe93d1276ba3b6e3ddedab9d2443
Mateo <mchavez.castillo23@gmail.com>
2024-09-02 15:33:42 -0500

    change in place buy order and add the function place sell order

--- src/com/globant/controller/OrderController.java
+++ src/com/globant/controller/OrderController.java
@@ -7,6 +7,8 @@ import com.globant.service.OrderService;
 import com.globant.service.UserService;
 import com.globant.view.ConsoleView;
 
+import java.util.Objects;
+
 public class OrderController {
 
     private final ConsoleView view;
@@ -24,28 +26,47 @@ public class OrderController {
         String[] data = view.buyExchange();
         String crypto = data[0];
         double amount = Double.parseDouble(data[1]);
-        if (crypto.equals(bitCoin.getName())){
-            double price = bitCoin.getPrice() * amount;
-            if (user.getWalletBalance() > price){
-                userService.balanceDeduct(user, price);
-                orderService.addBitCoin(user, price, amount);
-                view.showSuccess("Sell order successful");
-            }
+        int result = orderService.buyExachange(crypto, amount, bitCoin, ethereum, user);
+        if(result == 1){
+            view.showSuccess("Buy Exchange Success");
         }
-        if (crypto.equals(ethereum.getName())){
-            double price = ethereum.getPrice() * amount;
-            if (user.getWalletBalance() > price){
-                userService.balanceDeduct(user, price);
-                orderService.addEthereum(user, price, amount);
-                view.showSuccess("Sell order successful");
-            }
+        if(result == 0){
+            view.showError("Sorry, a error has occurred");
         }
     }
 
-    public void placeBuyOrder(double bitCoinPrice, double ethereumPrice) {
+    public void placeBuyOrder(User user, double bitCoinPrice, double ethereumPrice) {
         String[] data = view.placeBuyOrderPage(bitCoinPrice, ethereumPrice);
+        int result = orderService.placeBuyOrder(data, user);
+        if(result == 1){
+            view.showSuccess("Congratulations! A matching sell order has been found for your buy order. The transaction has been processed successfully");
+        }
+        if(result == 0){
+            view.showError("Sorry, no matching sell order was found for your buy order at this time. Please try again later or adjust your order criteria");
+        }
     }
 
+
+     public void placeSellOrder(BitCoin bitCoin, Ethereum ethereum, User user){
+        String[] data = view.placeSellOrderPage(bitCoin.getPrice(), ethereum.getPrice());
+        if(Objects.equals(data[0], "BitCoin")){
+            try{
+                orderService.placeSellOrder(data, user, bitCoin);
+                view.showSuccess("Place sell order successful");
+            }catch(Exception e){
+                view.showError("Place Sell Order Error");
+            }
+        }
+        if(Objects.equals(data[0], "Ethereum")){
+            try{
+                orderService.placeSellOrder(data, user, ethereum);
+                view.showSuccess("Place sell order successful");
+            }catch(Exception e){
+                view.showError("Place Sell Order Error");
+            }
+        }
+
+     }
 
 
 }
